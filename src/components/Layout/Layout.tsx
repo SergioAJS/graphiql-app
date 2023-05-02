7e6adcff4c176bc19a9d7d769e35c1949b166f95
SergioAJS <103531486+SergioAJS@users.noreply.github.com>
2023-05-02 10:20:43 +0500

    refactor: import alias

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,7 +1,7 @@
 import { Outlet } from 'react-router-dom';
-import { Footer } from '../Footer/Footer';
-import { Header } from '../Header/Header';
-import Container from '../Container/Container';
+import { Header } from 'components/Header/Header';
+import { Footer } from 'components/Footer/Footer';
+import Container from 'components/Container/Container';
 
 export const Layout = () => {
   return (
