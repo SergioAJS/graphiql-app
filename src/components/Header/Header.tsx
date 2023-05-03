9731f93efd8fa91bf02ab0cd94fb590c1c9ce70e
SergioAJS <103531486+SergioAJS@users.noreply.github.com>
2023-05-03 12:17:56 +0500

    feat: add ability to choose language

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,23 @@
 import { CustomLink } from 'components/CustomLink/CustomLink';
 import { ReactComponent as Logo } from 'assets/logo.svg';
+import { useState } from 'react';
+import { Language } from 'models/Language';
+import { enumToArray } from 'utils/enumToArray';
 
 export const Header = () => {
+  const [langOpen, setLangOpen] = useState(false);
+  const [lang, setCurrentLang] = useState(Language.en);
+  const languageList = enumToArray(Language);
+
+  const langHoverHandler = () => {
+    setLangOpen((prev) => !prev);
+  };
+
+  const onLangChoose = (language: Language) => {
+    setCurrentLang(language);
+    setLangOpen(false);
+  };
+
   return (
     <header className="flex w-full items-center justify-between gap-5 border-b-2 bg-white px-10 py-6 font-semibold">
       <div className="flex items-end justify-center gap-1 font-medium">
@@ -31,7 +47,35 @@ export const Header = () => {
             <button className="header-button">Sign Up</button>
           </CustomLink>
         </div>
-        <div className="text-center">Lang</div>
+        <div className="text-center">
+          <li
+            className="relative z-10 flex cursor-pointer flex-col items-center justify-center p-2 duration-300 hover:bg-blue-200"
+            onMouseEnter={langHoverHandler}
+            onMouseLeave={langHoverHandler}
+          >
+            <p>{Language[lang].toUpperCase()}</p>
+            <ul className="absolute top-full w-full bg-blue-500 text-white">
+              {langOpen &&
+                languageList.map((langCode, index) => (
+                  <li key={index}>
+                    <div
+                      onClick={() => onLangChoose(langCode)}
+                      onKeyDown={() => onLangChoose(langCode)}
+                      role="button"
+                      tabIndex={0}
+                      className={
+                        lang === Language[langCode]
+                          ? 'pointer-events-none bg-gray-500 p-2 text-white duration-300'
+                          : 'p-2 duration-300 hover:bg-blue-600'
+                      }
+                    >
+                      {Language[langCode].toUpperCase()}
+                    </div>
+                  </li>
+                ))}
+            </ul>
+          </li>
+        </div>
       </div>
     </header>
   );
