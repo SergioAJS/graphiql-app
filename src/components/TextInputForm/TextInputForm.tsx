8ab939e6ae395d4868a54d7842edd0803ff87186
SergioAJS <103531486+SergioAJS@users.noreply.github.com>
2023-05-07 07:26:25 +0500

    feat: translate validation

--- src/components/TextInputForm/TextInputForm.tsx
+++ src/components/TextInputForm/TextInputForm.tsx
@@ -23,6 +23,7 @@ export const TextInputForm = ({
   errors,
 }: InputProps) => {
   const { t } = useTranslation('textInputForm');
+
   return (
     <label className="block">
       <span
@@ -37,12 +38,14 @@ export const TextInputForm = ({
         {...register(name)}
         className={`mt-1 block w-full rounded-md border border-slate-300 bg-white px-3 py-2 text-slate-900 placeholder-slate-400 shadow-sm focus:border-sky-500 focus:outline-none focus:ring-1 focus:ring-sky-500 sm:text-base
           ${
-            errors[label] &&
+            errors[name] &&
             'border-pink-500 text-pink-600  focus:border-pink-500 focus:ring-pink-500'
           }`}
         placeholder={placeholder}
       />
-      {errors[label] && <p className="mt-2 text-sm text-pink-600">{errors[label]?.message}</p>}
+      {errors[name] && (
+        <p className="mt-2 text-sm text-pink-600">{t(`${errors[name]?.message}`)}</p>
+      )}
     </label>
   );
 };
