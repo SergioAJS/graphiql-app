fe4ba4110373a65300bd80c0742592143505f51f
Yuri Skripal <yjura@protonmail.com>
2023-05-10 15:51:15 +0300

    feat: add tab panel

--- src/pages/GraphiqlPage/GraphiqlPage.tsx
+++ src/pages/GraphiqlPage/GraphiqlPage.tsx
@@ -1,67 +1,108 @@
-import { useState } from 'react';
+import { ChangeEvent, useState } from 'react';
 import CodeEditor from '@uiw/react-textarea-code-editor';
+import { TabPanel, useTabs } from 'react-headless-tabs';
 
 import { Button } from 'components/Button/Button';
-import { useFetchGraphQuery } from 'redux/useFetchGraphQuery';
+import { QueryProps, useFetchGraphQuery } from 'redux/useFetchGraphQuery';
+import { TabSelector } from 'components/TabSelector/TabSelector';
 
-const EDITOR_HEIGHT = 700;
+const EDITOR_STYLES = {
+  className: 'w-auto',
+  language: 'graphql',
+  style: {
+    fontSize: 12,
+    backgroundColor: '#f5f5f5',
+    fontFamily: 'ui-monospace,SFMono-Regular,SF Mono,Consolas,Liberation Mono,Menlo,monospace',
+  },
+  padding: 15,
+};
 
-const GraphiqlPage = () => {
-  const [code, setCode] = useState(`query QueryReactions ($first: Int) {
-    reactions(first: $first) {
-      edges {
-        node {
-          Equation
-          reactionEnergy
-        }
+const DEFAULT_VARS = { first: 20 };
+
+const DEFAULT_QUERY = `query QueryReactions ($first: Int) {
+  reactions(first: $first) {
+    edges {
+      node {
+        Equation
+        reactionEnergy
       }
     }
   }
-  `);
-  const [query, setQuery] = useState(code);
-  const { data, isLoading, error } = useFetchGraphQuery({ query, variables: { first: 20 } });
+}
+`;
+
+const GraphiqlPage = () => {
+  const [selectedTab, setSelectedTab] = useTabs(['Variables', 'Headers']);
+  const [query, setQuery] = useState(DEFAULT_QUERY);
+  const [variables, setVariables] = useState(JSON.stringify(DEFAULT_VARS));
+  const [headers, setHeaders] = useState('');
+  const [graphQuery, setGraphQuery] = useState<QueryProps>({
+    variables: DEFAULT_VARS,
+    query: DEFAULT_QUERY,
+  });
+  const { data, isLoading, error } = useFetchGraphQuery(graphQuery);
   const handleQuery = () => {
-    setQuery(code);
+    try {
+      const parsedVariables = JSON.parse(variables);
+      const parsedHeaders = JSON.parse(headers);
+      setGraphQuery({ variables: parsedVariables, query, headers: parsedHeaders });
+    } catch {
+      /* empty */
+    }
   };
 
   return (
-    <div className="relative mt-16 flex h-[calc(80vh-64px)] flex-grow">
+    <div className="relative mt-16 flex h-[calc(80vh-16rem)] flex-grow">
       <div className="absolute left-2/4 top-1 z-20 -translate-x-1/2">
         <Button onClick={handleQuery}>Query</Button>
       </div>
       <div className="relative flex w-full" style={{ backgroundColor: '#f5f5f5' }}>
-        <CodeEditor
-          className="w-1/2 overflow-auto border border-b-0"
-          value={code}
-          language="graphql"
-          onChange={(evn) => {
-            setCode(evn.target.value);
-          }}
-          padding={15}
-          minHeight={EDITOR_HEIGHT}
-          style={{
-            fontSize: 12,
-            backgroundColor: '#f5f5f5',
-            fontFamily:
-              'ui-monospace,SFMono-Regular,SF Mono,Consolas,Liberation Mono,Menlo,monospace',
-          }}
-        />
+        <div className="w-1/2 overflow-auto border border-b-0">
+          <CodeEditor
+            value={query}
+            onChange={(evn: ChangeEvent<HTMLTextAreaElement>) => {
+              setQuery(evn.target.value);
+            }}
+            {...EDITOR_STYLES}
+          />
+          <nav className="border-b border-gray-300">
+            <TabSelector
+              isActive={selectedTab === 'Variables'}
+              onClick={() => setSelectedTab('Variables')}
+            >
+              Variables
+            </TabSelector>
+            <TabSelector
+              isActive={selectedTab === 'Headers'}
+              onClick={() => setSelectedTab('Headers')}
+            >
+              Headers
+            </TabSelector>
+          </nav>
+          <div>
+            <TabPanel hidden={selectedTab !== 'Variables'}>
+              <CodeEditor
+                value={variables}
+                onChange={(evn: ChangeEvent<HTMLTextAreaElement>) => {
+                  setVariables(evn.target.value);
+                }}
+                {...EDITOR_STYLES}
+              />
+            </TabPanel>
+            <TabPanel hidden={selectedTab !== 'Headers'}>
+              <CodeEditor
+                value={headers}
+                onChange={(evn: ChangeEvent<HTMLTextAreaElement>) => {
+                  setHeaders(evn.target.value);
+                }}
+                {...EDITOR_STYLES}
+              />
+            </TabPanel>
+          </div>
+        </div>
         <div className="relative w-1/2 overflow-auto border border-b-0 ">
           {isLoading && <Loading />}
-          <CodeEditor
-            className=" w-auto"
-            value={error || data}
-            readOnly
-            language="graphql"
-            padding={15}
-            minHeight={EDITOR_HEIGHT}
-            style={{
-              fontSize: 12,
-              backgroundColor: '#f5f5f5',
-              fontFamily:
-                'ui-monospace,SFMono-Regular,SF Mono,Consolas,Liberation Mono,Menlo,monospace',
-            }}
-          />
+          <CodeEditor value={error || data} readOnly {...EDITOR_STYLES} />
         </div>
       </div>
     </div>
