5f4da67b1bacc6b316d0b2dd99fcc62400217c18
Yuri Skripal <yjura@protonmail.com>
2023-05-23 16:36:56 +0300

    feat: add translate loading

--- src/components/Loading/Loading.tsx
+++ src/components/Loading/Loading.tsx
@@ -1,14 +1,19 @@
 import { ReactNode } from 'react';
+import { useTranslation } from 'react-i18next';
 
 type Props = {
   className?: string;
   children?: ReactNode;
 };
 
-export const Loading = ({ className, children = 'Loading...' }: Props) => (
-  <div
-    className={`text-2xl relative z-10 flex flex-col justify-center bg-gray-300 text-center ${className}`}
-  >
-    {children}
-  </div>
-);
+export const Loading = ({ className, children = 'Loading...' }: Props) => {
+  const { t } = useTranslation('loading');
+
+  return (
+    <div
+      className={`text-2xl relative z-10 flex flex-col justify-center bg-gray-300 text-center ${className}`}
+    >
+      {typeof children === 'string' ? t(children) : children}
+    </div>
+  );
+};
