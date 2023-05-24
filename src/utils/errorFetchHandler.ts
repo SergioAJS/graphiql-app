d80217aeb4767db5d6ddb9a92c38f6eb7b9991c0
Yuri Skripal <yjura@protonmail.com>
2023-05-24 19:03:22 +0300

    fix: error stringify options

--- src/utils/errorFetchHandler.ts
+++ src/utils/errorFetchHandler.ts
@@ -5,7 +5,7 @@ export const errorFetchHandler = (error: FetchBaseQueryError | SerializedError) 
   if (error) {
     if ('status' in error) {
       // you can access all properties of `FetchBaseQueryError` here
-      const errMsg = 'error' in error ? error.error : JSON.stringify(error.data);
+      const errMsg = 'error' in error ? error.error : JSON.stringify(error.data, null, '\t');
       return errMsg;
     } else {
       // you can access all properties of `SerializedError` here
